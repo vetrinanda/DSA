e8ba0683488f35720afa62d6c623f0400150a534
Nanda Kumar <nk9219382@gmail.com>
2026-01-24 23:21:36 +0530

    Implement solution for Single Number problem; define singleNumber method in problem16.py

--- problem16.py
+++ problem16.py
@@ -23,3 +23,13 @@ Input: nums = [1]
 
 Output: 1
 '''
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result

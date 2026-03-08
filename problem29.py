40c3100800507ff19f4d4f4f42c502ec5412398b
Nanda Kumar <nk9219382@gmail.com>
2026-03-08 17:15:01 +0530

    Implement rotate method to rotate an array to the right by k steps

--- problem29.py
+++ problem29.py
@@ -24,3 +24,16 @@ Explanation:
 rotate 1 steps to the right: [99,-1,-100,3]
 rotate 2 steps to the right: [3,99,-1,-100]'''
 
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n           # ← handles k larger than array size
+        for _ in range(k):
+            last = nums.pop()
+            nums.insert(0, last)

f496e4b1cdf0b74d96cb8999d93bba701b0243f8
Nanda Kumar <nk9219382@gmail.com>
2026-03-08 17:06:59 +0530

    Implement majorityElement method to find the majority element in an array

--- problem28.py
+++ problem28.py
@@ -18,5 +18,13 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2'''
 
+from ast import List
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+            if d[i] > len(nums) // 2:  # majority = appears more than n/2 times
+                return i
         

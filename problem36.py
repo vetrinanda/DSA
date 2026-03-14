4cf2cadfc13de66ce040f85bd058cfa7e291771c
Nanda Kumar <nk9219382@gmail.com>
2026-03-14 23:26:37 +0530

    Add problem description and examples for Longest Palindromic Substring

---
+++ problem36.py
@@ -0,0 +1,20 @@
+'''5. Longest Palindromic Substring
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ '''
